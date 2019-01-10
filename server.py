f04c18e3d01b48c342cb8910761e4e5461dddbc4
Celia Song <songcelia88@gmail.com>
2019-01-09 23:15:13 -0800

    add homepage, search and movie detail routes. just need to make the movie details page look nice

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request, flash, session, jsonify, json
 from flask_debugtoolbar import DebugToolbarExtension
+import requests
 
 
 app = Flask(__name__)
@@ -10,13 +11,70 @@ app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
+API_KEY = os.environ.get('API_KEY')
+MOVIE_URL = "https://api.themoviedb.org/3"
 
+# get the basic configuration parameters especially for the image urls
+config_url = MOVIE_URL+"/configuration?api_key="+API_KEY
+config = requests.get(config_url)
+config = config.json()
+IMG_SIZE = 'w342'
+IMG_URL = config['images']['secure_base_url'] + IMG_SIZE
 
 @app.route('/')
 def homepage():
+    """ Show the homepage and most popular movies today """
 
-    return("hello meow")
+    payload = {
+        'api_key': API_KEY,
+        'page': 1
+    }
+    endpoint_url = MOVIE_URL + "/movie/popular"
+    resp = requests.get(
+        endpoint_url,
+        params=payload)
 
+    results = resp.json()
+    # print(results['results'])
+
+    return render_template("homepage.html", movies=results['results'], imgUrl=IMG_URL, searched=False)
+
+
+@app.route('/search')
+def search_page():
+    """ Show the search results from the page """
+    keyword = request.args.get('keyword')
+
+    payload = {
+        'api_key': API_KEY,
+        'query': keyword,
+        'page': 1
+    }
+    endpoint_url = MOVIE_URL + "/search/movie"
+    resp = requests.get(
+        endpoint_url,
+        params=payload
+        )
+    results = resp.json()
+
+    return render_template("homepage.html", movies=results['results'], imgUrl=IMG_URL, searched=True)
+
+@app.route('/movie/<movie_id>')
+def show_movie_details(movie_id):
+    """ Show the details of the movie """
+
+    payload = {
+        'api_key': API_KEY,
+    }
+    endpoint_url = MOVIE_URL + "/movie/" + movie_id
+    resp = resp = requests.get(
+        endpoint_url,
+        params=payload
+    )
+    results = resp.json()
+    print(results)
+
+    return render_template("movie-details.html", movie=results, imgUrl=IMG_URL)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
@@ -25,6 +83,6 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = app.debug
     
     # Use the DebugToolbar
-    # DebugToolbarExtension(app)
+    DebugToolbarExtension(app)
 
     app.run(host='0.0.0.0')
